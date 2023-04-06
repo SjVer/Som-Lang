fa0b0c0c0b04a9350407b9e1cdc9cfb610d8e11d
SjVer <sjoerd@marsenaar.com>
2023-04-06 17:50:28 +0200

    import resolution rewritten

--- somc/lib/somc.ml
+++ somc/lib/somc.ml
@@ -2,7 +2,6 @@ module Analysis = Analysis
 module Codegen = Codegen
 module Configs = Configs
 module Parse = Parse
-module Pipeline = Pipeline
 module Report = Report
 module Span = Span
 module Typing = Typing
